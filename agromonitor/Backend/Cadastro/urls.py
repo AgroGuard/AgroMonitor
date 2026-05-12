4bcced19de15c1fbf3a2c20e5a0daf8bed1b4a29
AgroGuard <Agro0Guard@gmail.com>
2026-05-12 12:10:10 -0300

    Delete agromonitor/Backend directory

--- agromonitor/Backend/Cadastro/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_api, cadastro_api
-
-urlpatterns = [
-    path('login/', login_api, name='login_api'),
-    path('cadastro/', cadastro_api, name='cadastro_api'),
-]
-
