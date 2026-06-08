3d2869b79f07ec1d63c51c6cf0c2fb1ef372552e
AgroGuard <Agro0Guard@gmail.com>
2026-06-08 09:41:50 -0300

    Delete agromonitor/Backend directory

--- agromonitor/Backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('Cadastro.urls')),
-    path('api/sensores/', include('sensores.urls')),
-    path('dashboard/', include('Monitor.urls')),
-]
