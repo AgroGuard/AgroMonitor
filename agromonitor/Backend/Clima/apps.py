919998ae3c00130be126e8fb8fbd97f900a41efe
AgroGuard <Agro0Guard@gmail.com>
2026-06-10 17:50:14 -0300

    Delete agromonitor/Backend directory

--- agromonitor/Backend/Clima/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClimaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Clima'
-    verbose_name = 'Clima e Previsão do Tempo'
